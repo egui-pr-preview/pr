de90da8b84690a42622a9bf31bf775749a708423
emilk <emilk@users.noreply.github.com>
2026-09-10 11:13:50 +0000

    Update preview for PR 8529-emilkcode-completion-example

--- 8529-emilkcode-completion-example/egui_demo_app.js
+++ 8529-emilkcode-completion-example/egui_demo_app.js
@@ -2951,7 +2951,7 @@ let wasm_bindgen = (function(exports) {
                 return ret;
             },
             __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-                // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 5322, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+                // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 5324, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
                 const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h562a5388c9d921c2);
                 return ret;
             },
